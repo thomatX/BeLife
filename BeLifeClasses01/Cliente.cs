cf51a688db3f05b84ecf700ede49d6d32448aa6c
thomatX <tweisskapp@gmail.com>
2018-03-31 22:26:12 -0300

    Nuevo Repositorio
    Nuevo repositorio de proyecto creado.

---
+++ BeLifeClasses01/Cliente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeLifeClasses01
+{
+    public class Cliente
+    {
+
+        public string Rut { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public int EstadoCivil { get; set; }
+        public int Sexo { get; set; }
+
+        public Cliente() {
+            this.InitClass();
+        }
+
+        private void InitClass() {
+            this.Rut = String.Empty;
+            this.Nombres = String.Empty;
+            this.Apellidos = String.Empty;
+            this.FechaNacimiento = DateTime.Today;
+            this.EstadoCivil = 0;
+            this.Sexo = 0;
+        }
+
+    }
+}
---
+++ BeLifeClasses01/Contrato.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeLifeClasses01
+{
+    class Contrato
+    {
+        public int NumeroContrato { get; set; }
+        public DateTime Creacion { get; set; }
+        public DateTime Termino { get; set; }
+        public Cliente Titular { get; set; }
+        public Plan PlanAsociado { get; set; }
+        public int Poliza { get; set; }
+        public bool EstaVigente { get; set; }
+
+        public DateTime InicioVigencia
+        {
+            get
+            {
+                return Creacion;
+            }
+            set { }
+        }
+        public DateTime FinVigencia
+        {
+            get {
+                return Termino;
+            }
+
+            set { }
+        }
+
+
+    }
+}
